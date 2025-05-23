188f31278c9ab4c5174f0214a16caf8b64a1bb75
gRoussac <gRoussac@users.noreply.github.com>
2025-05-23 13:40:22 +0000

    Deploy documentation for dev-1.6

--- juliet/api-rust/trait.impl/core/hash/trait.Hash.js
+++ juliet/api-rust/trait.impl/core/hash/trait.Hash.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["casper_rust_wasm_sdk",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"casper_rust_wasm_sdk/types/cl/bytes/struct.Bytes.html\" title=\"struct casper_rust_wasm_sdk::types::cl::bytes::Bytes\">Bytes</a>"]]]]);
+    var implementors = Object.fromEntries([["casper_rust_wasm_sdk",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"casper_rust_wasm_sdk/types/cl/bytes/struct.Bytes.html\" title=\"struct casper_rust_wasm_sdk::types::cl::bytes::Bytes\">Bytes</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
