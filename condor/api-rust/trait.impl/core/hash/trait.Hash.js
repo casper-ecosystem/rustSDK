7a1450bc95c55e49ca489ca6ed19ecb34218cd70
gRoussac <gRoussac@users.noreply.github.com>
2026-03-16 17:33:41 +0000

    Deploy documentation for dev

--- condor/api-rust/trait.impl/core/hash/trait.Hash.js
+++ condor/api-rust/trait.impl/core/hash/trait.Hash.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["casper_rust_wasm_sdk",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.1/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"casper_rust_wasm_sdk/types/cl/bytes/struct.Bytes.html\" title=\"struct casper_rust_wasm_sdk::types::cl::bytes::Bytes\">Bytes</a>"]]]]);
+    const implementors = Object.fromEntries([["casper_rust_wasm_sdk",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"casper_rust_wasm_sdk/types/cl/bytes/struct.Bytes.html\" title=\"struct casper_rust_wasm_sdk::types::cl::bytes::Bytes\">Bytes</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[324]}
+//{"start":59,"fragment_lengths":[326]}
--- condor/api-rust/trait.impl/core/ops/deref/trait.Deref.js
+++ condor/api-rust/trait.impl/core/ops/deref/trait.Deref.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["casper_rust_wasm_sdk",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.1/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"casper_rust_wasm_sdk/types/cl/bytes/struct.Bytes.html\" title=\"struct casper_rust_wasm_sdk::types::cl::bytes::Bytes\">Bytes</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.1/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"casper_rust_wasm_sdk/types/uref/struct.URef.html\" title=\"struct casper_rust_wasm_sdk::types::uref::URef\">URef</a>"]]]]);
+    const implementors = Object.fromEntries([["casper_rust_wasm_sdk",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.0/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"casper_rust_wasm_sdk/types/cl/bytes/struct.Bytes.html\" title=\"struct casper_rust_wasm_sdk::types::cl::bytes::Bytes\">Bytes</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.0/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"casper_rust_wasm_sdk/types/uref/struct.URef.html\" title=\"struct casper_rust_wasm_sdk::types::uref::URef\">URef</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[638]}
+//{"start":59,"fragment_lengths":[642]}
