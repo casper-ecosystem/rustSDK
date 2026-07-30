df5c227c75756393dee4ba8b5a307f22cdecb841
gRoussac <gRoussac@users.noreply.github.com>
2026-07-30 21:06:37 +0000

    Deploy documentation for dev

--- condor/api-rust/trait.impl/core/hash/trait.Hash.js
+++ condor/api-rust/trait.impl/core/hash/trait.Hash.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["casper_rust_wasm_sdk",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"casper_rust_wasm_sdk/types/cl/bytes/struct.Bytes.html\" title=\"struct casper_rust_wasm_sdk::types::cl::bytes::Bytes\">Bytes</a>",0]]]]);
+    const implementors = Object.fromEntries([["casper_rust_wasm_sdk",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.97.1/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"casper_rust_wasm_sdk/types/cl/bytes/struct.Bytes.html\" title=\"struct casper_rust_wasm_sdk::types::cl::bytes::Bytes\">Bytes</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
