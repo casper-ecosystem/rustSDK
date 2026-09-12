a7e36b0e6b6f14030f190686851cd1552c3a2611
gRoussac <gRoussac@users.noreply.github.com>
2026-09-12 09:13:07 +0000

    Deploy documentation for dev

--- api-rust/trait.impl/core/convert/trait.AsMut.js
+++ api-rust/trait.impl/core/convert/trait.AsMut.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["casper_rust_wasm_sdk",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.98.0/core/convert/trait.AsMut.html\" title=\"trait core::convert::AsMut\">AsMut</a>&lt;<a class=\"struct\" href=\"https://docs.rs/casper-types/7.1.1/casper_types/transaction/runtime_args/struct.RuntimeArgs.html\" title=\"struct casper_types::transaction::runtime_args::RuntimeArgs\">RuntimeArgs</a>&gt; for <a class=\"struct\" href=\"casper_rust_wasm_sdk/types/runtime_args/struct.RuntimeArgs.html\" title=\"struct casper_rust_wasm_sdk::types::runtime_args::RuntimeArgs\">RuntimeArgs</a>",0]]]]);
+    const implementors = Object.fromEntries([["casper_rust_wasm_sdk",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.98.1/core/convert/trait.AsMut.html\" title=\"trait core::convert::AsMut\">AsMut</a>&lt;<a class=\"struct\" href=\"https://docs.rs/casper-types/7.1.1/casper_types/transaction/runtime_args/struct.RuntimeArgs.html\" title=\"struct casper_types::transaction::runtime_args::RuntimeArgs\">RuntimeArgs</a>&gt; for <a class=\"struct\" href=\"casper_rust_wasm_sdk/types/runtime_args/struct.RuntimeArgs.html\" title=\"struct casper_rust_wasm_sdk::types::runtime_args::RuntimeArgs\">RuntimeArgs</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
